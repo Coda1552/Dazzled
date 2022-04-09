f0823a273c056f218bd341bf572ef86f8d259822
Cody <cody.koelliker@outlook.com>
2022-04-09 04:52:02 -0600

    more gnome work (does not compile)

--- src/main/java/coda/overtherainbow/common/entities/GnomeEntity.java
+++ src/main/java/coda/overtherainbow/common/entities/GnomeEntity.java
@@ -4,18 +4,24 @@ import coda.overtherainbow.registry.OTRItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.util.VisibleForDebug;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.PanicGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.util.AirRandomPos;
 import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimationTickable;
 import software.bernie.geckolib3.core.PlayState;
@@ -26,6 +32,7 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class GnomeEntity extends PathfinderMob implements IAnimatable, IAnimationTickable {
     private static final int TOO_FAR_DISTANCE = 32;
@@ -74,6 +81,119 @@ public class GnomeEntity extends PathfinderMob implements IAnimatable, IAnimatio
         super.readAdditionalSaveData(p_27793_);
         this.stayOutOfHomeCountdown = p_27793_.getInt("CannotEnterHomeTicks");
     }
+
+    void pathfindRandomlyTowards(BlockPos p_27881_) {
+        Vec3 vec3 = Vec3.atBottomCenterOf(p_27881_);
+        int i = 0;
+        BlockPos blockpos = this.blockPosition();
+        int j = (int)vec3.y - blockpos.getY();
+        if (j > 2) {
+            i = 4;
+        } else if (j < -2) {
+            i = -4;
+        }
+
+        int k = 6;
+        int l = 8;
+        int i1 = blockpos.distManhattan(p_27881_);
+        if (i1 < 15) {
+            k = i1 / 2;
+            l = i1 / 2;
+        }
+
+        Vec3 vec31 = AirRandomPos.getPosTowards(this, k, l, i, vec3, (double)((float)Math.PI / 10F));
+        if (vec31 != null) {
+            this.navigation.setMaxVisitedNodesMultiplier(0.5F);
+            this.navigation.moveTo(vec31.x, vec31.y, vec31.z, 1.0D);
+        }
+    }
+
+    @VisibleForDebug
+    public int getTravellingTicks() {
+        return Math.max(this.goToHomeGoal.travellingTicks, this.goToKnownFlowerGoal.travellingTicks);
+    }
+
+    @VisibleForDebug
+    public List<BlockPos> getBlacklistedHomes() {
+        return this.goToHomeGoal.blacklistedTargets;
+    }
+
+    boolean wantsToEnterHome() {
+        if (this.stayOutOfHomeCountdown <= 0 && !this.beePollinateGoal.isPollinating() && !this.hasStung() && this.getTarget() == null) {
+            boolean flag = this.isTiredOfLookingForNectar() || this.level.isRaining() || this.level.isNight() || this.hasNectar();
+            return flag && !this.isHomeNearFire();
+        } else {
+            return false;
+        }
+    }
+
+    public void setStayOutOfHomeCountdown(int p_27916_) {
+        this.stayOutOfHomeCountdown = p_27916_;
+    }
+
+    private boolean doesHomeHaveSpace(BlockPos p_27885_) {
+        BlockEntity blockentity = this.level.getBlockEntity(p_27885_);
+        if (blockentity instanceof BeehomeBlockEntity) {
+            return !((BeehomeBlockEntity)blockentity).isFull();
+        } else {
+            return false;
+        }
+    }
+
+    @VisibleForDebug
+    public boolean hasHome() {
+        return this.homePos != null;
+    }
+
+    @Nullable
+    @VisibleForDebug
+    public BlockPos getHomePos() {
+        return this.homePos;
+    }
+
+    @VisibleForDebug
+    public GoalSelector getGoalSelector() {
+        return this.goalSelector;
+    }
+
+    public void aiStep() {
+        super.aiStep();
+        if (!this.level.isClientSide) {
+            if (this.stayOutOfHomeCountdown > 0) {
+                --this.stayOutOfHomeCountdown;
+            }
+
+            if (this.remainingCooldownBeforeLocatingNewHome > 0) {
+                --this.remainingCooldownBeforeLocatingNewHome;
+            }
+
+            if (this.remainingCooldownBeforeLocatingNewFlower > 0) {
+                --this.remainingCooldownBeforeLocatingNewFlower;
+            }
+
+            boolean flag = this.isAngry() && !this.hasStung() && this.getTarget() != null && this.getTarget().distanceToSqr(this) < 4.0D;
+            this.setRolling(flag);
+            if (this.tickCount % 20 == 0 && !this.isHomeValid()) {
+                this.homePos = null;
+            }
+        }
+
+    }
+
+    boolean isHiveValid() {
+        if (!this.hasHive()) {
+            return false;
+        } else {
+            BlockEntity blockentity = this.level.getBlockEntity(this.hivePos);
+            return blockentity instanceof BeehiveBlockEntity;
+        }
+    }
+
+    boolean isTooFarAway(BlockPos p_27890_) {
+        return !this.closerThan(p_27890_, 32);
+    }
+
+    // TODO - keep working (line 619 in Bee.java)
 
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 10.0D).add(Attributes.MOVEMENT_SPEED, 0.3D);
